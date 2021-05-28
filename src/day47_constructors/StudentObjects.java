e789ebc01736ba62fdc2847c1636da495b5e27c8
Misha Antic <mishaantic17@gmail.com>
2021-05-27 22:20:13 -0400

    May 8th update

---
+++ src/day47_constructors/StudentObjects.java
@@ -0,0 +1,14 @@
+package day47_constructors;
+
+public class StudentObjects {
+    public static void main(String[] args) {
+        Student st1 = new Student();
+        Student st2 = new Student();
+
+        Student st3 = new Student("Misha");
+        Student st4 = new Student("Anta");
+
+        Student st5 = new Student("Misha Antic", 37);
+
+    }
+}
