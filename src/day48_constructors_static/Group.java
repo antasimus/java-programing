a9180b6522c63247c49c03bad3790d88b4f2cd03
Misha Antic <mishaantic17@gmail.com>
2021-05-29 14:20:59 -0400

    May29th2ndCommit

---
+++ src/day48_constructors_static/Group.java
@@ -0,0 +1,38 @@
+package day48_constructors_static;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Group {
+    private String name;
+    private List<String> members = new ArrayList<>();
+
+    public void addMember(String newMember){
+        members.add(newMember);
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<String> getMembers() {
+        return members;
+    }
+
+    public void setMembers(List<String> members) {
+        this.members = members;
+    }
+
+    @Override
+    public String toString() {
+        return "Group{" +
+                "name='" + name + '\'' +
+                ", members=" + members +
+                '}';
+    }
+}
